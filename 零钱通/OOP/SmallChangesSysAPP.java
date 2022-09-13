b78fddea886f791db6fac387b388f4e123f3374f
左国杨 <112050152+uozuo@users.noreply.github.com>
2022-09-13 17:17:38 +0800

    Create SmallChangesSysAPP.java

---
+++ 零钱通/OOP/SmallChangesSysAPP.java
@@ -0,0 +1,12 @@
+package 零钱通.OOP;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class SmallChangesSysAPP {
+    public static void main(String[] args) {
+        SmallChangeOOP smallChange=new SmallChangeOOP();
+        smallChange.MainMenu();
+    }
+}
